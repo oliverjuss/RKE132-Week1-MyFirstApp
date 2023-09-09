62521c99130c0ec7b8817fa4f4cac48ed81319b3
Oliver Juss <oliverjuss007@gmail.com>
2023-09-09 17:36:03 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// This is a comment to my code
+
+// rakendus küsib kasutajal sisestada tema nime
+// rakendus tervitab teda
+
+Console.WriteLine("Tere mis on sinu nimi");
+string nimi = Console.ReadLine();
+
+Console.WriteLine($"Hello, {nimi} !");
